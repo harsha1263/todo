3aa6dd1f589a71791e370e168d9b0c96bdd302f9
harsha1263 <harshavardhan63@gmail.com>
2026-05-10 11:57:22 +0530

    Allocated host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gw2b94n**9-gbpggwy@%=0fecno44k^!i87&%leiv%pvg-t*&f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.109.60.140', 'localhost', '127.0.0.1']
 
 
 # Application definition
